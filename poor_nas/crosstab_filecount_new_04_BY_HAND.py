40a7a00d31389fcb53ad25a66d9e3fe3dbf89c96
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2024-07-31 14:41:07 +0930

    snapshot before omitting entries for stuff which has zero files

--- poor_nas/crosstab_filecount_new_04_BY_HAND.py
+++ poor_nas/crosstab_filecount_new_04_BY_HAND.py
@@ -276,7 +276,7 @@ def main():
         mergerfs_disks_in_LtoR_order_from_fstab,
         mergerfs_disks_having_a_root_folder
     )
-    common_functions.log_and_print("Unique Top-Level Media Folders" :", data=unique_top_level_media_folders)
+    common_functions.log_and_print("Unique Top-Level Media Folders :", data=unique_top_level_media_folders)
     common_functions.debug_pause()
 
     # Step 4: Generate and log the crosstab report
