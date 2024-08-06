491565c30b03810ffeeb204d903637a1aec9fe59
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2024-08-06 13:22:04 +0930

    Add files via upload

--- README.md
+++ README.md
@@ -547,11 +547,11 @@ sudo mkdir -v -m a=rwx /srv
 # {1..8} for 8 disks, for underlying file system that mergerfs will depend on. More than we use is OK.
 sudo mkdir -v -m 777 /srv/usb3disk{1..8}
 #
+# mount point for mergerfs to present the consolidated underlying file systems
+sudo mkdir -v -m a=rwx /srv/media
+#
 # double-ensure the protections are as we want them by setting them on the tree
 sudo chmod -R -v a+rwx /srv
-#
-# moount point for mergerfs to present the consolidated underlying file systems
-sudo mkdir -v -m a=rwx /srv/media
 #
 # Notes:
 # /srv will be shared 'rw' by SAMBA to provide access to the underlying file systems (particularly the 'ffd's)
--- make_mountpoints.sh
+++ make_mountpoints.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 set -x
 #
+# /srv probably already exists, try to create it anyway
 sudo mkdir -v -m a=rwx /srv
+#
+# {1..8} for 8 disks, for underlying file system that mergerfs will depend on. More than we use is OK.
+sudo mkdir -v -m 777 /srv/usb3disk{1..8}
+#
+# mount point for mergerfs to present the consolidated underlying file systems
+sudo mkdir -v -m a=rwx /srv/media
+#
+# double-ensure the protections are as we want them by setting them on the tree
 sudo chmod -R -v a+rwx /srv
 #
-# for mergerfs to present the consolidated underlying file systems
-sudo mkdir -v -m a=rwx /srv/mediafs
-#
-# for underlying file system for mergerfs
-sudo mkdir -v -m a=rwx /srv/usb3disk1
-sudo mkdir -v -m a=rwx /srv/usb3disk2
-sudo mkdir -v -m a=rwx /srv/usb3disk3
-sudo mkdir -v -m a=rwx /srv/usb3disk4
-sudo mkdir -v -m a=rwx /srv/usb3disk5
-sudo mkdir -v -m a=rwx /srv/usb3disk6
-sudo mkdir -v -m a=rwx /srv/usb3disk7
-#
+# Notes:
 # /srv will be shared 'rw' by SAMBA to provide access to the underlying file systems (particularly the 'ffd's)
-# /srv/mediafs will be shared 'rw' by SAMBA to provide access to the mergerfs merged disks
+# /srv/media will be shared 'rw' by SAMBA to provide access to the mergerfs merged disks
 #
+ls -al /srv/
 set +x
